c781d6358b3223eb29b4b674c8c3fc99931fdb60
Rushikesh <sorushikesh07@gmail.com>
2025-11-15 17:04:25 +0530

    Code 0001 : Two Sum

---
+++ src/main/java/com/java_board/core/leetCode/Code0001TwoSum.java
@@ -0,0 +1,31 @@
+package com.java_board.core.leetCode;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Code0001TwoSum {
+
+  public static void main(String[] args) {
+
+    int[] numbs = {2,7,11,15};
+    int target = 9;
+    log.info("Indices of the two numbers : {}", twoSum(numbs, target));
+  }
+
+  public static int[] twoSum(int[] numbs, int target) {
+    int[] result = new int[2];
+
+    for (int i = 0; i < numbs.length; i++) {
+      int temp = target - numbs[i];
+      for (int j = i + 1; j < numbs.length; j++) {
+        if (numbs[j] == temp) {
+          result[0] = i;
+          result[1] = j;
+          return result;
+        }
+      }
+    }
+
+    return result;
+  }
+}
